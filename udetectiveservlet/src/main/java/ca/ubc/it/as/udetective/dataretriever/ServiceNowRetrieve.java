976df87c8951c81f1f35c0824a17360c303e605e
Armenak Grigoryan <armenak.grigoryan@ub.ca>
2017-06-14 12:52:36 -0700

    Move hardcoded period of time for data refresh from the code to the property file.

--- udetectiveservlet/src/main/java/ca/ubc/it/as/udetective/dataretriever/ServiceNowRetrieve.java
+++ udetectiveservlet/src/main/java/ca/ubc/it/as/udetective/dataretriever/ServiceNowRetrieve.java
@@ -1,30 +1,28 @@
 package ca.ubc.it.as.udetective.dataretriever;
 
-import ca.ubc.it.as.udetective.inputds.ElasticSearchDataSource;
-import ca.ubc.it.as.udetective.inputds.IDataSource;
-import ca.ubc.it.as.udetective.inputds.ServiceNowDataSource;
-import ca.ubc.it.as.udetective.model.AbstractModel;
 import java.util.Arrays;
 import java.util.List;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpMethod;
-import org.apache.commons.httpclient.methods.GetMethod;
-import org.apache.commons.lang.StringUtils;
-
 import org.apache.log4j.Logger;
 import static org.apache.log4j.Logger.getLogger;
 
 import com.google.gson.Gson;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.lang.StringUtils;
 
 import ca.ubc.it.as.udetective.model.ServiceNowTicket;
 import ca.ubc.it.as.udetective.utils.AppProperties;
 import ca.ubc.it.as.udetective.service.ClaimService;
 import ca.ubc.it.as.udetective.utils.Utilities;
 
+import ca.ubc.it.as.udetective.inputds.ElasticSearchDataSource;
+import ca.ubc.it.as.udetective.inputds.IDataSource;
 
 /**
  * Retrieves data from ServiceNow
@@ -38,20 +36,14 @@ public class ServiceNowRetrieve implements IRetriever {
     @Override
     public void retrieve(IDataSource snDataSource) throws IOException {
                                 
-//        HttpClient client = new HttpClient();
-//        client.getParams().setAuthenticationPreemptive(false);
-//        Credentials creds = new UsernamePasswordCredentials(AppProperties.getProperty("username"), AppProperties.getProperty("password"));
-//        client.getState().setCredentials(AuthScope.ANY, creds);
-
-        //IDataSource snDataSource = new ServiceNowDataSource();
         HttpClient client = (HttpClient)snDataSource.connect();
         
         // We should use an encoded query in this case because URL parameters do not support the full semantics of a filter.        
         String serviceUrl     = AppProperties.getProperty("service_url");
         String restPath       = AppProperties.getProperty("rest_path");
         String resultLimit    = AppProperties.getProperty("result_limit");
-        String hardCodedForTesting = "48";
-        String url1           = "?sysparm_query=active%3Dtrue%5Estate!%3D6%5Esys_created_onRELATIVEGE%40hour%40ago%40" + hardCodedForTesting;
+        String period_of_time = AppProperties.getProperty("period_of_time");
+        String url1           = "?sysparm_query=active%3Dtrue%5Estate!%3D6%5Esys_created_onRELATIVEGE%40hour%40ago%40" + period_of_time;
         String url2           = "%5Eassignment_group%3D81db147e2b5c79444dde23f119da153b&sysparm_display_value=true&sysparm_fields=sys_id%2Cnumber%2Cshort_description%2Cdescription&sysparm_limit=" + resultLimit;
         
         String finalUrl = serviceUrl + restPath + url1 + url2;
